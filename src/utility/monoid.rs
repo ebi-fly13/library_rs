bc0a1a3345afda01d874621fcdf4c7331748c376
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2022-02-22 19:54:42 +0900

    impl Sum

--- src/utility/monoid.rs
+++ src/utility/monoid.rs
@@ -1,3 +1,7 @@
+use std::ops::Add;
+use std::marker::PhantomData;
+use num::Zero;
+
 pub trait Monoid {
     type S: Clone;
     fn e() -> Self::S;
@@ -9,4 +13,19 @@ pub trait MapMonoid: Monoid {
     fn id() -> Self::F;
     fn mapping(f: &Self::F, s: &Self::S) -> Self::S;
     fn composition(f: &Self::F, g: &Self::F) -> Self::F;
+}
+
+pub struct Sum<T>(PhantomData<fn() -> T>);
+
+impl<T> Monoid for Sum<T>
+where 
+T: Zero + Add + Copy
+{
+    type S = T;
+    fn e() -> Self::S {
+        Self::S::zero()
+    }
+    fn op(lhs: &Self::S, rhs: &Self::S) -> Self::S {
+        *lhs + *rhs
+    }
 }
