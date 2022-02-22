af746a80c631f97a09146f93735f470df4e25685
ebi-fly13 <yano.h.ag@m.titech.ac.jp>
2022-02-22 18:48:20 +0900

    add impl's model

--- src/data_structure/lazysegtree.rs
+++ src/data_structure/lazysegtree.rs
@@ -2,10 +2,44 @@ use crate::utility::monoid::MapMonoid;
 
 pub struct LazySegTree<M>
 where
-    M: MapMonoid, 
+    M: MapMonoid,
 {
     n: usize,
     size: usize,
     data: Vec<M::S>,
     lazy: Vec<M::F>,
+}
+
+impl<M> LazySegTree<M>
+where
+    M: MapMonoid,
+{
+    pub fn new(n: usize) -> Self {
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+        Self {
+            n,
+            size,
+            data: vec![M::e(); 2 * size],
+            lazy: vec![M::id(); size],
+        }
+    }
+
+    pub fn set(&mut self, idx: usize, x: M::S) {}
+
+    pub fn get(&self, idx: usize) -> M::S {
+        M::e()
+    }
+
+    pub fn prod(&self, l: usize, r: usize) -> M::S {
+        M::e()
+    }
+
+    pub fn all_prod(&self) -> M::S {
+        M::e()
+    }
+
+    pub fn apply(&mut self, l: usize, r: usize, f: M::F) {}
 }
